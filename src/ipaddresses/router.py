61d8077099cd9dd9ae5e4f88cc545f726778f4e4
kudomuro <kudomuro@gmail.com>
2024-06-26 14:52:19 +0500

    Добавлена функция создания ip адреса, добавлены функции вывода данных БД из таблиц: spaces, iptypes, modes, nettypes, vrfs, equiptypes

--- src/ipaddresses/router.py
+++ src/ipaddresses/router.py
@@ -20,7 +20,7 @@ router = APIRouter(
 
 @router.get("/get_ip", response_model=List[GetIpaddress])
 async def get_specific_ipaddresses(get_ip: IPv4Interface, user_auth = Depends(current_user), session: AsyncSession = Depends(get_async_session)):
-    query = select(ipaddress.c.code, spaces.c.title.label('space'), ipaddress.c.ip, ipaddress.c.mask, iptypes.c.title.label('iptype'), modes.c.title.label('mode'), nettypes.c.title.label('nettype'), vrfs.c.title.label('vrf'), ipaddress.c.title, equiptypes.c.title.label('equiptype'), objects.c.title.label('obj'), user.c.username.label('lmuser'), ipaddress.c.lmdate).\
+    query = select(ipaddress.c.code, spaces.c.title.label('space'), ipaddress.c.ip, ipaddress.c.mask, iptypes.c.title.label('iptype'), modes.c.title.label('mode'), nettypes.c.title.label('nettype'), vrfs.c.title.label('vrf'), ipaddress.c.title, equiptypes.c.title.label('equiptype'), objects.c.title.label('obj'), user.c.username.label('lmuser'), ipaddress.c.lmdate, ipaddress.c.parent_id).\
         select_from(
             ipaddress.outerjoin(iptypes, ipaddress.c.iptype==iptypes.c.code).\
                 join(spaces, ipaddress.c.space==spaces.c.code).\
@@ -53,21 +53,21 @@ async def get_specific_ipaddresses(parent: int = None, session: AsyncSession = D
     tree = insert_children(dict_result)
     return tree
 
-@router.get("/get_ip", response_model=List[GetIpaddress])
-async def get_specific_ipaddresses(get_ip: IPv4Interface, user_auth = Depends(current_user), session: AsyncSession = Depends(get_async_session)):
-    query = select(ipaddress.c.code, spaces.c.title.label('space'), ipaddress.c.ip, ipaddress.c.mask, iptypes.c.title.label('iptype'), modes.c.title.label('mode'), nettypes.c.title.label('nettype'), vrfs.c.title.label('vrf'), ipaddress.c.title, equiptypes.c.title.label('equiptype'), objects.c.title.label('obj'), user.c.username.label('lmuser'), ipaddress.c.lmdate).\
-        select_from(
-            ipaddress.outerjoin(iptypes, ipaddress.c.iptype==iptypes.c.code).\
-                join(spaces, ipaddress.c.space==spaces.c.code).\
-                outerjoin(modes, ipaddress.c.mode==modes.c.code).\
-                outerjoin(nettypes, ipaddress.c.nettype==nettypes.c.code).\
-                outerjoin(vrfs, ipaddress.c.vrf==vrfs.c.code).\
-                outerjoin(equiptypes, ipaddress.c.equiptype==equiptypes.c.code).\
-                outerjoin(objects, ipaddress.c.obj==objects.c.code).\
-                outerjoin(user, ipaddress.c.lmuser==user.c.id)
-        ).where(ipaddress.c.ip == get_ip).order_by(ipaddress.c.ip)
-    result = await session.execute(query)
-    return result.all()
+# @router.get("/get_ip", response_model=List[GetIpaddress])
+# async def get_specific_ipaddresses(get_ip: IPv4Interface, user_auth = Depends(current_user), session: AsyncSession = Depends(get_async_session)):
+#     query = select(ipaddress.c.code, spaces.c.title.label('space'), ipaddress.c.ip, ipaddress.c.mask, iptypes.c.title.label('iptype'), modes.c.title.label('mode'), nettypes.c.title.label('nettype'), vrfs.c.title.label('vrf'), ipaddress.c.title, equiptypes.c.title.label('equiptype'), objects.c.title.label('obj'), user.c.username.label('lmuser'), ipaddress.c.lmdate).\
+#         select_from(
+#             ipaddress.outerjoin(iptypes, ipaddress.c.iptype==iptypes.c.code).\
+#                 join(spaces, ipaddress.c.space==spaces.c.code).\
+#                 outerjoin(modes, ipaddress.c.mode==modes.c.code).\
+#                 outerjoin(nettypes, ipaddress.c.nettype==nettypes.c.code).\
+#                 outerjoin(vrfs, ipaddress.c.vrf==vrfs.c.code).\
+#                 outerjoin(equiptypes, ipaddress.c.equiptype==equiptypes.c.code).\
+#                 outerjoin(objects, ipaddress.c.obj==objects.c.code).\
+#                 outerjoin(user, ipaddress.c.lmuser==user.c.id)
+#         ).where(ipaddress.c.ip == get_ip).order_by(ipaddress.c.ip)
+#     result = await session.execute(query)
+#     return result.all()
 
 @router.get("/get_ip_table/{ip_id}", response_model=List[CreateIpaddress])
 async def get_specific_ipaddresses(ip_id: int, user_auth = Depends(current_user), session: AsyncSession = Depends(get_async_session)):
@@ -220,8 +220,9 @@ async def get_specific_ipaddresses(ip_id: int, user_auth = Depends(current_user)
 async def add_specific_ipaddresses(new_ip: CreateIpaddress, user_auth = Depends(current_user), session: AsyncSession = Depends(get_async_session)):
     enter_ip = new_ip.ip
     # Проверка существует ли указанный ip
-    query_check_ip = await session.execute(select(ipaddress).filter(ipaddress.c.ip == enter_ip))
+    query_check_ip = await session.execute(select(ipaddress.c.ip).where(ipaddress.c.ip == enter_ip))
     check_ip = query_check_ip is None # Если false, значит такого адреса нет
+    print("Указанный адрес существует:", check_ip, query_check_ip)
     # Поиск родительской сети для адреса
     # subquery_find_parent = select([text(f"'{enter_ip}'::inet AS new_ip")]).alias("new_address")
     async def find_parent(enter_ip):
@@ -235,36 +236,42 @@ async def add_specific_ipaddresses(new_ip: CreateIpaddress, user_auth = Depends(
         result = await session.execute(query, new_address)
         return result.fetchone()    
     
-    async def find_count_child(parent_network_res):
+    async def find_count_child(parent_network_id):
         query_count_child = (
-           select(ipaddress.c.parent_id)
-            .filter(ipaddress.c.parent_id == parent_network_res)
+           select(func.count(ipaddress.c.parent_id))
+            .filter(ipaddress.c.parent_id == parent_network_id)
         )
-        # query_count_child = ("SELECT COUNT(*) FROM ipaddress WHERE parent_id = '{parent_network_res}' GROUP BY parent_id;")
+        # query_count_child = ("SELECT COUNT(*) FROM ipaddress WHERE parent_id = '{parent_network_id}' GROUP BY parent_id;")
         result = await session.execute(query_count_child)
-        count_result = result.all()
-        # print(query_count_child)
+        count_result = result.first()
         return count_result
     
     parent_network = await find_parent(enter_ip)
-    parent_network_res = parent_network[0]
+    print("Родительская сеть:", parent_network)
+    parent_network_id = parent_network[0]
+    print("Родительская сеть parent_network_id:", parent_network_id)
     parent_network_mask = parent_network[3]
+    print("Родительская сеть parent_network_mask:", parent_network_mask)
 
-    # if (not check_ip):
-    #     count_child = await find_count_child(parent_network_res)
-    #     count_child_res = len(count_child)
-    #     percent_use = (count_child_res*100)/(2**(32-parent_network_mask)-2)        
-    #     if (percent_use<100):
-    #         stmt = insert(ipaddress).values(**new_ip.dict())
-    #         await session.execute(stmt)
-    #         await session.commit()
-    #         return {'status': 'success'}
-    #     else:
-    #         return {'status': 'network_full'}
-    # else:
-    #     return {'status': 'address_exists'}
-    new_ip.parent_id=parent_network_res
-    print(new_ip.ip.prefixlen)
-    # print('Количество: ',count_child_res)
+    if (not check_ip):
+        count_child = await find_count_child(parent_network_id)
+        print('Количество потомков: ', count_child[0])
+        # count_child_res = len(count_child)
+        percent_use = (count_child[0]*100)/(2**(32-parent_network_mask)-2)        
+        if (percent_use<100):
+            stmt = insert(ipaddress).values(**new_ip.dict())
+            await session.execute(stmt)
+            # await session.commit()
+            print("stmt=", stmt)
+            return {'status': 'success'}
+        else:
+            return {'status': 'network_full'}
+    else:
+        return {'status': 'address_exists'}
+
+    # new_ip.parent_id=parent_network_id
+    # print(new_ip.ip.prefixlen)
+
+    print('Количество потомков: ',await find_count_child(parent_network_id))
     # print('Процент использования: ',percent_use)
     return {'status': 'success'}
